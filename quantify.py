be3f740e500aece59ce7faa64a374e9ef7f00b22
Miguel Martin Abadal <mmabadal@gmail.com>
2019-11-18 11:43:46 +0100

    add print

--- quantify.py
+++ quantify.py
@@ -2,6 +2,7 @@
 
 import os
 import re
+import matplotlib.pyplot as plt
 import sys
 import argparse
 import numpy as np
@@ -34,34 +35,31 @@ def getPredictions(file):
 
 def main():
 
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument('--path_in', help='txt input directory.')
-    # parser.add_argument('--path_out', help='txt output directory.')
-    # parser.add_argument('--cthr', default = 0, help='if eval 0, cthr to get quantification')
-    # parser.add_argument('--print', default = 0, help='0 -> no print, 1 -> print quant (and gt)')
-    # parser.add_argument('--wsize', default = 8, help='window size')
-    # parser.add_argument('--wover', default = 0.25, help='window overlap')
-    # parsed_args = parser.parse_args(sys.argv[1:])
+    # python quantify.py --path_in "test" --path_out "" --cthr 0.5 --print 1  --wsize 8 --wover 0.5
 
-    # path_in = parsed_args.path_in
-    # path_out = parsed_args.path_out
-    # cthr = float(parsed_args.cthr)
-    # print = int(parsed_args.print)
-    # wsize = int(parsed_args.wsize)
-    # wover = float(parsed_args.wover)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--path_in', help='txt input directory.')
+    parser.add_argument('--path_out', default="", help='txt output directory.')
+    parser.add_argument('--cthr', default=0, help='if eval 0, cthr to get quantification')
+    parser.add_argument('--print', default=0, help='0 -> no print, 1 -> print quant (and gt)')
+    parser.add_argument('--wsize', default=8, help='window size')
+    parser.add_argument('--wover', default=0.25, help='window overlap')
+    parsed_args = parser.parse_args(sys.argv[1:])
 
-    path_in = "test"
-    # path_out = ""
-    cthr = 0.5
-    print = 1
-    wsize = 8
-    wover = 0.5
-    wover = int(wover*wsize)
+    path_in = parsed_args.path_in
+    path_out = parsed_args.path_out
+    cthr = float(parsed_args.cthr)
+    print = int(parsed_args.print)
+    wsize = int(parsed_args.wsize)
+    wover = float(parsed_args.wover)
+
+    wover_ip = int(wover*wsize)
 
     predictions_list = list()
 
-    # if not os.path.exists(path_out):
-    #    os.makedirs(path_out)
+    if path_out != "":
+        if not os.path.exists(path_out):
+            os.makedirs(path_out)
 
     # read predictions
     for file in sorted(os.listdir(path_in)):
@@ -110,38 +108,40 @@ def main():
 
     for i, name in enumerate(u_classes):
         counts = np.choose(i, preds_count.T)
-        counts_win = view_as_windows(counts, wsize, step=wover)
+        counts_win = view_as_windows(counts, wsize, step=wover_ip)
 
         for j, win in enumerate(counts_win):
             values, rep = np.unique(win, return_counts=True)
-            val = win[np.argmax(rep)]
+            val = values[np.argmax(rep)]
             preds_count_win[j, i] = val
 
     # expand window quantification to all initial information points
-    ip = n_counts*(wsize-wover) + wover
+    ip = n_counts*(wsize-wover_ip) + wover_ip
     quantifications = np.zeros((ip, n_classes), dtype=int)
 
     for i, name in enumerate(u_classes):
         counts_win = np.choose(i, preds_count_win.T)
-        quant = np.repeat(counts_win, wsize-wover)
-        for j in range(wover):
+        quant = np.repeat(counts_win, wsize-wover_ip)
+        for j in range(wover_ip):
             quant = np.insert(quant, 0, quant[0])
         quantifications[..., i] = quant
 
-    # save quantifications to csv
+    path_save_csv = os.path.join(path_out, "quant"+"_"+str(wsize)+"_"+str(wover_ip)+"_"+str(cthr)+"_"+".csv")
+    np.savetxt(path_save_csv, quantifications)  # save quantifications to csv
 
     if print == 1:
-        z = 0
-        # print
 
-    z = 0
+        fig = plt.figure()
+        plt.xlabel('information points')
+        plt.ylabel('jellyfish counts')
+        x = np.arange(0, quantifications.shape[0] , 1, dtype=int)
+        for i, name in enumerate(u_classes):
+            plt.plot(x, quantifications[..., i], label=name)
 
+        plt.legend(loc='upper right', frameon=False)
 
-
-
-
-
-
+        path_save_plot = os.path.join(path_out, "quant"+"_"+str(wsize)+"_"+str(wover_ip)+"_"+str(cthr)+"_"+".png")
+        plt.savefig(path_save_plot)  # save quantifications to plot
 
 
 main()
